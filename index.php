5bbd0003374090110739de8c93cc382b48e1b50b
Richard Rodrigues <richardrodrfr@gmail.com>
2023-09-05 12:02:55 -0300

    Add files via upload
    Site simples utilizando html e CSS.

---
+++ index.php
@@ -0,0 +1,117 @@
+<doctype html>
+	<html lang="pt-br" =>
+	<head>
+		<meta charset="UTF-8">
+		<title> Montador de Móveis BHZ</title>
+		<link rel="stylesheet" href="estilo.css">
+
+		<
+
+	</head>
+	<body>
+
+		<header>
+			<h1>Montador de moveis BHZ!</h1>
+			<h2>Montagem e desmontagem de móveis </h2>
+		</header>
+
+
+
+
+			 <div id="nav">
+		<a href="#inicio">Inicio</a>
+        <a href="#orcamentos">Orçamentos</a>
+        <a href="#fotos">Fotos</a>
+        <a href="#quem_somos">Quem somos</a>
+        <a href="#servicos">Serviços</a>
+        <a href="#contato">Contato</a>
+        <a href="https://wa.me/5531981023629">Whatsapp montador</a>
+    </div>
+    </table>
+
+
+
+	<body>
+		
+
+
+		<h1 id="inicio">
+			<br><br>
+		Sejam bem vindos.Somos os montadores de móveis bhz!.</h1>
+		<br><br>
+		<img src="https://cdn.desapega.net/pictures/ce/7a7b13f6e4b33fe4336828790b6b8d3057a474592e462528c0496c4d159c87-dc32b3.jpg" width="900" height="600">
+		<br><br>
+
+		<h2 id="fotos">
+		<span class="destaque">Fotos de Serviços</span>
+			<br><br>
+		<br><br>
+		<img src="https://img.olx.com.br/images/43/430328542940758.webp" width="500">
+		<br><br>
+
+		<img src="imagens/imagemsite.jpg" width="500">
+		<br><br>
+		<img src="https://img.olx.com.br/images/34/343358551934910.webp"width="500">
+		<br><br>
+		<img src="imagens/foto02.jpg" width="500">
+		<br><br>
+		<img src="imagens/foto04.jpg" width="500">
+		<br><br>
+		<img src="imagens/foto06.jpg" width="500">
+		<br><br>
+	</h2>
+
+	
+	</h3>
+	<br><br>
+
+		<h4>
+		
+		<br><br>
+
+		
+	</h4>
+	<br><br>
+
+	<h5 id="orcamentos">
+		<br><br><br><br><br><br>
+		<span class="destaque"> <br>Orçamento:<br> </span>
+		Orçamento pelo telefone ou whatsapp:31 98102-3629.<br> 
+		Richard.
+
+		instagram @richardrockwater.
+		<br><br>
+
+		</h5>
+		<br><br>
+
+		<h6 id="quem_somos">
+		<span class="destaque"><br>Quem somos<br>
+		</span>
+		<br><br>
+	</h6>
+	<h7 id="servicos">
+		<span class="destaque"><br> Serviços<br></span>
+		<br>
+		Montagem de móveis, desmontagem, instalações e serviços comerciais e residenciais.
+		<br>
+		Belo horizonte e regiao metropolitana.
+		<br> orçamentos pelo telefone ou whatsapp: 31 98102-3629. Richard.
+		<br>
+		Profissionais de confiança, parceiros sustentaveis e patrocinadores do projeto horta vida e resistencia.
+		<br><br>
+
+		</h7>
+		<br><br>
+
+	</body>
+	<img src="https://avatars.githubusercontent.com/u/136639623?s=400&u=3f611f9906e3a1186f12cb632476a587891aabab&v=4">
+		
+		
+
+		<footer>
+    <h2 id="contato">Contato:31-98102-3629<br></h2>
+    
+    <p>&copy; 2023 Richard Rodrigues. Todos os direitos reservados.</p>
+</footer>
+	</html>
